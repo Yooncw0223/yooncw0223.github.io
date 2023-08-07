787529868994dfe0cefd6b9382b02b88c6ebc346
Yooncw0223 <yooncw0223@gmail.com>
2023-08-07 01:28:38 -0400

    navbar and home page roughly done; plan on filling the About page

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import logo from './assets/images/catLogo2.gif';
 import './App.css';
 import Navbar from './components/navbar';
 
+const ABOUT = lazy(() => import('./pages/about'));
+const HOME = lazy(() => import('./pages/home'));
+
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
     <div className="App">
       <Navbar/>
+
+      <Suspense fallback={<>Loading...</>}>
+        
+        <Routes>
+          <Route path="/" element={<HOME/>}/>
+          <Route path="/about" element={<ABOUT/>}/>
+        </Routes>
+
+      </Suspense>
     </div>
   );
 }
 
-export default App;
+function AppWithRouter() {
+  return (
+    <Router>
+      <App />
+    </Router>
+  )
+}
+
+export default AppWithRouter;
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,36 +1,43 @@
+
 import './navbar.css';
+import logo from '../assets/images/catLogo2.gif';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <header>
-    <div>
-    
+    <header className="nav-header">
+      <div className="headerLeft">
+
+        <img src={logo} className="App-logo" />
+        <h1 className="siteTitle"> Yooncw0223 </h1>
+
+      </div>
+
       <nav className="navBar">
         
         <ul className="navList">
           <li className="navBarElement">
-            <a href="#"> Home </a>
+            <Link to="/"> Home </Link>
           </li>
 
           <li className="navBarElement">
-            <a href="#"> Reflections </a>
+            <Link to="/reflections"> Reflections </Link>
           </li>
 
           <li className="navBarElement">
-            <a href="#"> Projects </a>
+            <Link to="/projects"> Projects </Link>
           </li>
 
-          <li className="navBarElement">
-            <a href="#"> Miscellaneous </a>
-          </li>
+            <li className="navBarElement">
+              <Link to="/misc"> Miscellaneous </Link>
+            </li>
 
-          <li className="navBarElement">
-            <a href="#"> About </a>
-          </li>
-        </ul>
-      </nav>
-
-    </div>
+            <li className="navBarElement">
+              <Link to="/about"> About </Link>
+            </li>
+          </ul>
+        </nav>
     </header>
+
   );
 }
